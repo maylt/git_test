d8789e44dd56ba13a3c06090751f969f8946f796
maylt <maylt86@gmail.com>
2019-06-25 15:34:07 +0700

    docker tomcat template

---
+++ ca-docker-tomcat_template-mlt/main.tf
@@ -0,0 +1,31 @@
+provider "docker" {
+  host = "http://10.243.41.201:4243"
+}
+
+resource "docker_container" "mlt-tomcat-test" {
+  name  = "${random_string.random_name.result}-1"
+  image = "dong91/automic_tomcat-01"
+  entrypoint = ["/bin/sleep"]
+  command = [ "1d" ]
+   
+  ports {
+   internal = "22"
+   external = "9001"
+   protocol = "tcp"
+  
+  }
+  
+  ports {
+   internal = "8080"
+   external = "9000"
+   protocol = "tcp"
+  
+  }
+}
+
+resource "random_string" "random_name" {
+	length  = 5
+	special = false
+	lower   = false
+}
+
